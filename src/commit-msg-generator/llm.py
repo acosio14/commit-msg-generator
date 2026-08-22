d49a45ad2230278ab0d4435d6ace1a917b34701a
Adrian Cosio <adriancosio14@gmail.com>
2026-08-22 05:56:11 -0500

    refactor: change from OpenAI client to Openrouter. Add various comments.

--- src/commit-msg-generator/llm.py
+++ src/commit-msg-generator/llm.py
@@ -1,4 +1,4 @@
-from openai import OpenAI
+from openrouter import OpenRouter
 from openrouter.errors import (
     BadRequestResponseError,
     UnauthorizedResponseError,
@@ -26,10 +26,16 @@ FATAL = (
     BadRequestResponseError,
     UnauthorizedResponseError,
     ForbiddenResponseError,
-)
-RETRY = (
-    json.JSONDecodeError,
     PayloadTooLargeResponseError, 
+)
+
+#Add Reroutes: TooManyRequestsResponseError, ProviderOverloadedResponseError, NotFoundResponseError, ResponseValidationError
+# Not handling feat(api) / feat!:, use regex for this
+# Need timeout error
+
+RETRY = (
+    CommitMsgError,
+    json.JSONDecodeError,
     TooManyRequestsResponseError,
     InternalServerResponseError,
     BadGatewayResponseError,
@@ -37,8 +43,8 @@ RETRY = (
 )
 
 class LLM:
-    def __init__(self, model: str = "poolside/laguna-s-2.1:free"):
-        self.default_model = [model]
+    def __init__(self, default_model: list[str] = ["poolside/laguna-s-2.1:free"]):
+        self.default_model = default_model
         self.fallback_models = [
             "z-ai/glm-5.2:free",
             "liquid/lfm-2.5-2.6b:free",
@@ -50,65 +56,70 @@ class LLM:
             ]
 
     def run_llm(self, git_diff: str, max_attempts: int = 4) -> dict[str]:
-        models = self.default_model.extend(self.fallback_models)
-        for model in models:
-            for attempt in range(max_attempts):
-                try:
-                    with OpenAI(
-                        base_url="https://openrouter.ai/api/v1",
-                        api_key=os.environ.get("OPENROUTER_API_KEY")
-                    ) as client:
-
-                        response = client.responses.create(
-                            model=self.model,
-                            instructions=(
-                                "You are a senior software engineer. " \
-                                "Specializing in git conventional commits " \
-                                "and common software standards. " \
-                                "You generate concise conventional commit messages " \
-                                "based ONLY on the given `git diff` context. " \
-                                "The commit messages should be prefixed with one of following types: " \
-                                "fix, feat, build, chore, ci, docs, style, refactor, perf, test. " \
-                                "Keep commit message under 150 characters. " \
-                                "Return a string in a json format structure, " \
-                                "example shown in the delimited triple backticks:\n" \
-                                f"""
-                                '
-                                {{
-                                "model": "{model}",
-                                "commit_message": "fix: remove bug"
-                                }}
-                                '
-                                """
-                                # Need error handling for when it doesn't return a json structure, retry
-                                # Or if commit message not less than 150 char, or doesn't have prefix type
-                            ),
-                            input=(
-                                f"Take the given git diff context in the delimited triple backticks and " \
-                                f"write a conventional commit message that summarizes all the changes " \
-                                f"being implemented in the code in one concise sentence. " \
-                                f"```{git_diff}```" \
-                            )
+        models = self.default_model + self.fallback_models
+        with OpenRouter(api_key=os.environ.get("OPENROUTER_API_KEY")) as client: 
+            for model in models:
+                for attempt in range(max_attempts):
+                    try:
+                        response = client.responses.send(
+                            model = model,
+                            response_format = { "type": "json_object"},
+                            messages = [
+                                {"role": "system", "content":
+                                    "You are a senior software engineer. " \
+                                    "Specializing in git conventional commits " \
+                                    "and common software standards. " \
+                                    "You generate concise conventional commit messages " \
+                                    "based ONLY on the given `git diff` context. " \
+                                    "The commit messages should be prefixed with one of following types: " \
+                                    "fix, feat, build, chore, ci, docs, style, refactor, perf, test. " \
+                                    "Keep commit message under 100 characters. " \
+                                    "Do not include the prefix in the commit message character limit." \
+                                    "Return a string in a json format structure, " \
+                                    "example shown in the delimited triple backticks:\n" \
+                                    f"""
+                                    '
+                                    {{
+                                    "model": "{model}",
+                                    "commit_message": "fix: remove bug"
+                                    }}
+                                    '
+                                    """
+                                    # Need error handling for when it doesn't return a json structure, retry
+                                    # Or if commit message not less than 150 char, or doesn't have prefix type
+                                },
+                                {"role": "user",
+                                 "content":
+                                    f"Take the given git diff context in the delimited triple backticks and " \
+                                    f"write a conventional commit message that summarizes all the changes " \
+                                    f"being implemented in the code in one concise sentence. " \
+                                    f"```{git_diff}```" \
+                                }
+                            ]
                         )
-                    response_json =  json.loads(response.output_text)
 
-                    # if doesn't include prefix or less than 150 char -> fail
-                    prefix_list = ["fix", "feat", "build", "chore", "ci", "docs", "style", "refactor", "perf", "test"]
-                    prefix, msg = response_json["commit_message"].split(":")
-                    if prefix not in prefix_list:
-                        raise CommitMsgError # will it crash? Need it to retry
-                    
-                    if len(msg) > 150:
-                        raise CommitMsgError
+                        response_json =  json.loads(response.choices[0].message.content)
+                        # Not every model support response type - json. Need per-model capability flag or 
+                        # a graceful decay (if model doesn't support -> fallback to what?)
 
-                    return response_json
+                        # if doesn't include prefix or less than 150 char -> fail
+                        prefix_list = ["fix", "feat", "build", "chore", "ci", "docs", "style", "refactor", "perf", "test"]
+                        prefix, msg = response_json["commit_message"].split(":", 1)
+                        # The above could return KeyError if returns json with wrong shape (RETRY)
+                        # TypeError if returned as list or string instead of dict. Validate the shape before indexing.
+                        if prefix not in prefix_list:
+                            raise CommitMsgError # will this and the below one crash? Need it to retry
+                        
+                        if len(msg) > 100:
+                            raise CommitMsgError
 
-                except FATAL as e: # These are fatal errors
-                    raise LLMError
+                        return response_json
 
-                except RETRY as e: # These are retryable errors
-                    print(f"Error: {e}")
-                    time.sleep(2 ** attempt)
-                    continue
-        raise LLMError("All inputs exhausted")    
-            
+                    except FATAL as e: # These are fatal errors
+                        raise LLMError from e
+
+                    except RETRY as e: # These are retryable errors
+                        print(f"Error: {e}")
+                        time.sleep(2 ** attempt)
+                        continue
+        raise LLMError("All inputs exhausted")                
