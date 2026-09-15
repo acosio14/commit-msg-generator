7f92c8d9a04c3b6c5ebf77ce3df25b9b83f5f118
Adrian Cosio <adriancosio14@gmail.com>
2026-09-15 17:51:50 -0500

    feat: add name_status parsing and diff parsing

--- src/commit_msg_generator/parser.py
+++ src/commit_msg_generator/parser.py
@@ -1,6 +1,6 @@
+import re
 
-
-def run(diff_stat: str, diff_message: str):
+def run(diff_stat: str, diff_file_status: str, diff_message: str):
 
     stats = []
     for line in diff_stat.splitlines():
@@ -9,7 +9,13 @@ def run(diff_stat: str, diff_message: str):
             (int(added), int(deleted), str(filepath))
         )
 
-    # Do something with stats
-    
-    
-    
+    name_status = []
+    for line in diff_file_status.splitlines():
+        status, file = line.split()
+        name_status.append(
+            (status, file)
+        )
+
+    diff_files = re.split(r'(?=diff)', diff_message)
+    for file in diff_files:
+        header, hunk, content = re.split(r"(@@[^@]+@@\n)", file)
