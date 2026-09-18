881d93e496c5eef2cc712a3b3f49327499347b13
Adrian Cosio <adriancosio14@gmail.com>
2026-09-17 21:03:12 -0500

    feat: update methods to private and split diffs properly.

--- src/commit_msg_generator/parser.py
+++ src/commit_msg_generator/parser.py
@@ -3,22 +3,26 @@ from dataclasses import dataclass
 from typing import List
 
 @dataclass
+class DiffContent:
+    hunk: str
+    content: str
+
+@dataclass
 class FileDiff:
     file: str
     status: str
-    header: str
-    hunk: str
-    diff_content: str
     stats: tuple[str]
+    header: str
+    diffs: List[DiffContent]
 
 
 class DiffParser:
-    def __init__(self, num_stat: str, name_status: str, diff_message: str) -> None:
-        self.stat = num_stat
-        self.status = name_status
+    def __init__(self, num_stat_msg: str, name_status_msg: str, diff_message: str) -> None:
+        self.stat = num_stat_msg
+        self.status = name_status_msg
         self.diff_msg = diff_message
 
-    def get_stats(self):
+    def _get_stats(self):
         stats = []
         for line in self.stat.splitlines():
             added, deleted, filepath = line.split()
@@ -26,7 +30,7 @@ class DiffParser:
                 (int(added), int(deleted), str(filepath))
             )
 
-    def get_status(self):
+    def _get_status(self):
         name_status = []
         for line in self.status.splitlines():
             status, file = line.split()
@@ -34,10 +38,30 @@ class DiffParser:
                 (status, file)
             )
 
-    def get_diff_sections(self):
-        diff_files = re.split(r'(?=diff)', self.diff_msg)
-        for file in diff_files:
-            header, hunk, content = re.split(r"(@@[^@]+@@\n)", file)
+    def _get_diff_sections(self, file_section):
+        diff_content_list = []
+        for section in file_section:
+            header, file_diff = re.split(r"(?=@@[\d\s\+\-\,]+@@)", section, maxsplit=1)
 
+            diffs = re.split(r"(?=@@[\d\s\+\-\,]+@@)", file_diff)
+            for diff in diffs:
+                hunk, content = re.split(r"(?=@@[\d\s\+\-\,]+@@)", diff)
+                diff_content_list.append(DiffContent(hunk, content))
+
+        return header, diff_content_list
+
+    def _extract_filename(self, file_section):
+        # To-Do: Need to get filename parsed from here to go along with header, hunks, contents
+        ...
 
     def run(self) -> List[FileDiff]:
+        file_sections = re.split(r'(?=diff)', self.diff_msg)
+        name_status_list = self._get_status() # status, file
+        num_stats_list = self._get_stats() # added, deleted, filepath
+
+        for file_section in file_sections:
+            filename = self._extract_filename(file_section)
+            # To-Do: Change status and stats to dicts then I can use key/value pairs to extract values and store in dataclass
+            header, diff_content_list = self._get_diff_sections(file_section)
+
+            FileDiff(filename, status, stats, header, diff_content_list)
