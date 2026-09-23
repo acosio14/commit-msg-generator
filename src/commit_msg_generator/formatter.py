cf635eeef523f98e2db8e866792a4adc3dc180a6
Adrian Cosio <adriancosio14@gmail.com>
2026-09-23 05:57:01 -0500

    feat: add initial llm prompt and json converter functions to formatter.

--- src/commit_msg_generator/formatter.py
+++ src/commit_msg_generator/formatter.py
@@ -1,11 +1,24 @@
-from commit_msg_generator import git_commands
 import logging
+from commit_msg_generator.parser import FileDiff
+from dataclasses import asdict
+import json
 
 logger = logging.getLogger(__name__)
 
-class LLMPrompt:
-    def __init__()
+
+# from parser:
+# [ FileDiff(filename, status, stats, header, [ DiffContent(hunk, content) ]) ]
+    # Make a json formatted object?
+def convert_file_diff_to_json_format(list_file_diffs: FileDiff):
+    for file_diff in list_file_diffs:
+        file_diff_json = json.dumps(asdict(file_diff))
 
 
-    def generate() -> str:
-        ...
+def create_llm_prompt():
+
+    initial_prompt = (
+        "Using the information on the git diff below in the " \
+        "delimited triple backticks, write 3 conventional " \
+        "commit messages."
+    )
+
