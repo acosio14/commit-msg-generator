fbfe11481d23f43eab0d11a4f1af3e5e65339746
Adrian Cosio <adriancosio14@gmail.com>
2026-09-23 05:12:45 -0500

    feat: update main with git commands and parser class

--- src/commit_msg_generator/main.py
+++ src/commit_msg_generator/main.py
@@ -1,5 +1,6 @@
 import logging
-from . import git_commands, parser
+from . import git_commands, formatter
+from .parser import DiffParser
 
 def main():
     logging.basicConfig(
@@ -7,6 +8,10 @@ def main():
         format="%(asctime)s %(levelname)s: %(messages)s",
     )
 
+    diff_stats = git_commands.diff_num_stat()
+    diff_name_status = git_commands.diff_name_status()
     diff_message = git_commands.diff()
 
-    parsed_diff_msg = parser.run(diff_message)
+    list_of_file_diffs = DiffParser(diff_stats, diff_name_status, diff_message).run()
+
+    llm_prompt = formatter(list_of_file_diffs)
