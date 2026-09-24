e2f5e25fe482c70615a74865052fd9d57390f32d
Adrian Cosio <adriancosio14@gmail.com>
2026-09-24 05:36:39 -0500

    feat: add guardrail for empty git diff strings

--- src/commit_msg_generator/git_commands.py
+++ src/commit_msg_generator/git_commands.py
@@ -5,31 +5,35 @@ logger = logging.getLogger(__name__)
 
 
 def diff_num_stat():
-    # Get stats to see what changes, see if too large to deal with. (added/deleted count)
-    try:
-        return subprocess.run(["git", "diff", "--staged", "--numstat"], capture_output=True, text=True).stdout
-    except:
+    diff_stat = subprocess.run(["git", "diff", "--staged", "--numstat"], capture_output=True, text=True).stdout
+    if len(diff_stat) == 0:
         logger.exception(
-            "error: git diff stats"
+            "error: git diff stats returned empty string. "
+            "Verify that changes are staged."
         )
+    else:
+        return diff_stat
 
 def diff_name_status():
-    try:
-        return subprocess.run(["git", "diff", "--staged","--name-status"], capture_output=True, text=True).stdout
-    except:
+    diff_name_status = subprocess.run(["git", "diff", "--staged", "--name-status"], capture_output=True, text=True).stdout
+    if len(diff_name_status) == 0:
+        
         logger.exception(
-            "error: git diff name status"
+            "error: git diff name status returned empty string. "
+            "Verify that changes are staged."
         )
+    else:
+        return diff_name_status
 
 def diff() -> str:
-    # Show diff if staged
-    try:
-        return subprocess.run(["git", "diff", "--staged", "--unified=0"], capture_output=True, text=True).stdout
-    except:
+    diff_msg = subprocess.run(["git", "diff", "--staged", "--unified=0"], capture_output=True, text=True).stdout
+    if len(diff_msg) == 0:
         logger.exception(
-            "error: git diff return empty string."
-            "verify that changes are staged."
+            "error: git diff return empty string. "
+            "Verify that changes are staged."
         )
+    else:
+        return diff_msg
 
 def commit(msg: str) -> None:
     try:
@@ -39,4 +43,6 @@ def commit(msg: str) -> None:
             "error: commit msg"
         )
 
-    
+# To-Do: 
+# - Add unit test for git-commands
+# - Figure out how to combine all functions into one; right now they are repeating (if I change one I change all)
