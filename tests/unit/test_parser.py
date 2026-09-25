9c0c59d0121ad2698f5ed3ac2fc5da9b6d882801
Adrian Cosio <adriancosio14@gmail.com>
2026-09-25 17:32:36 -0500

    test: add good path unit test for DiffParser().run()

---
+++ tests/unit/test_parser.py
@@ -0,0 +1,42 @@
+import pytest
+from src.commit_msg_generator.parser import DiffParser, FileDiff, DiffContent
+
+"""
+Tests
+- 1 file modified / 1 change
+- 1 file modified / 2 changes
+- 1 file renamed
+- 1 file added
+- 1 file deleted
+- 2 files modified / 1 change each
+- 1 Very large file modified / 500 changes (uv.lock type)
+- empty git msg, num_stat, and status
+"""
+def test_run_one_file_one_line_modification_return_FileDiff():
+    # Arrange.
+    diff_msg = (
+        "diff --git a/src/example.py b/src/example.py"
+        "index f063189..a2dd04a 100644"
+        "--- a/src/example.py"
+        "+++ b/src/example.py"
+        "@@ -91,4 +91 @@ class Addition:"
+        "- a = 10"
+        "+ a = 11"
+    )
+    diff_parser = DiffParser("","", diff_msg)
+    expected_diff_class = FileDiff(
+        "src/example.py",
+        "",
+        "",
+        "diff -- a/src/example.py b/src/example.py\nindex f063189..a2dd04a 100644\n--- a/src/example.py\n+++ b/src/example.py",
+        DiffContent(
+            "@@ -91,4 +91 @@",
+            " class Addition:\n- a = 10\n+ a = 11"
+            )
+        )
+
+    # Act.
+    list_of_file_diff = diff_parser.run()
+
+    # Assert.
+    assert list_of_file_diff == expected_diff_class
